589d663b483dcf8cd77383734e915b6f536d48d9
Muslim <mkhudoyberdiyev98@gmail.com>
2021-09-04 23:36:51 +0300

    'retrutn_as_a_data'

--- main.py
+++ main.py
@@ -5,32 +5,28 @@ import sys
 
 
 def main(image):
+    data={}
     image,gray=read_image(image)
     frames=separate_contours(image,gray)
     test_id,first,second,third,fourth,fifth,thresh=separate_answers(frames)
     for i in range(0,6):
         if i==0:
             user_id=get_id(test_id,frames[i],thresh[i],TEST_ID)
-            print(user_id)
+            data["user_id"]=user_id
         if i==1:
-            print("FIRST_ANSWERS")
             first_answers=check_answers(first,frames[i],thresh[i],ANSWER_KEY_1)
-            print(first_answers)
+            data["first"]=first_answers
         if i==2:
-            print("SECOND_ANSWERS")
             second_answers=check_answers(second,frames[i],thresh[i],ANSWER_KEY_2) 
-            print(second_answers)
+            data["second"]=second_answers
         if i==3:
-            print("THIRD_ANSWERS")
-            third_answars=check_answers(third,frames[i],thresh[i],ANSWER_KEY_3) 
-            print(third_answars)
+            third_answers=check_answers(third,frames[i],thresh[i],ANSWER_KEY_3) 
+            data["third"]=third_answers
         if i==4:
-            print("FOURTH_ANSWERS")
             fourth_answers=check_answers(fourth,frames[i],thresh[i],ANSWER_KEY_4)
-            print(fourth_answers)
+            data["fourth"]=fourth_answers
         if i==5:
-            print("FIFTH_ANSWERS")
             fifth_aswers=check_answers(fifth,frames[i],thresh[i],ANSWER_KEY_5) 
-            print(fifth_aswers)
-
+            data["fifth"]=fifth_aswers
+    print([data])
 main(sys.argv[1])
