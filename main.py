342e7466e4a33e92c59de9c571dbce5c76b3242e
Muslim Khudoyberdiev <62444467+muslimkhudoyberdiev@users.noreply.github.com>
2021-09-11 11:26:42 +0300

    last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from prepocessing.preprocess import read_image,separate_answers,separate_contours,get_id,check_answers
-from prepocessing.check_result import *
 import sys
 
 
@@ -11,22 +10,22 @@ def main(image):
     test_id,first,second,third,fourth,fifth,thresh=separate_answers(frames)
     for i in range(0,6):
         if i==0:
-            user_id=get_id(test_id,frames[i],thresh[i],TEST_ID)
+            user_id=get_id(test_id,frames[i],thresh[i])
             data["user_id"]=user_id
         if i==1:
-            first_answers=check_answers(first,frames[i],thresh[i],ANSWER_KEY_1)
+            first_answers=check_answers(first,frames[i],thresh[i])
             data["first"]=first_answers
         if i==2:
-            second_answers=check_answers(second,frames[i],thresh[i],ANSWER_KEY_2) 
+            second_answers=check_answers(second,frames[i],thresh[i]) 
             data["second"]=second_answers
         if i==3:
-            third_answers=check_answers(third,frames[i],thresh[i],ANSWER_KEY_3) 
+            third_answers=check_answers(third,frames[i],thresh[i]) 
             data["third"]=third_answers
         if i==4:
-            fourth_answers=check_answers(fourth,frames[i],thresh[i],ANSWER_KEY_4)
+            fourth_answers=check_answers(fourth,frames[i],thresh[i])
             data["fourth"]=fourth_answers
         if i==5:
-            fifth_aswers=check_answers(fifth,frames[i],thresh[i],ANSWER_KEY_5) 
+            fifth_aswers=check_answers(fifth,frames[i],thresh[i]) 
             data["fifth"]=fifth_aswers
     print([data])
 main(sys.argv[1])
